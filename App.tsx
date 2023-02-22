f906efbab1c21ab43fe44c914ccda61e18bc560b
Dimitri CORREIA <125220634+dimitri-correia@users.noreply.github.com>
2023-02-22 23:20:42 +0100

    adding home screen to the navigator

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen } from "./screens/HomeScreen";
 
 const NativeStackNavigator = createNativeStackNavigator();
 
@@ -8,6 +9,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <NativeStackNavigator.Navigator initialRouteName="Home">
+        <NativeStackNavigator.Screen
+          name={"Home"}
+          component={HomeScreen}
+          // options={{ headerShown: false }}
+        />
       </NativeStackNavigator.Navigator>
     </NavigationContainer>
   );
