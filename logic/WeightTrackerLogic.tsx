50b33cfcfcd75a9eb7f0c883a0e162afd57f06d4
Dimitri CORREIA <125220634+dimitri-correia@users.noreply.github.com>
2023-02-23 23:24:29 +0100

    logic to compute the average weight

--- logic/WeightTrackerLogic.tsx
+++ logic/WeightTrackerLogic.tsx
@@ -3,3 +3,22 @@ export interface WeightEntry {
   date: Date;
   weight: number;
 }
+
+export const averageXdays = (
+  entries: WeightEntry[],
+  date: Date,
+  numberOfDays: number
+): number => {
+  const entriesInLastXDays: number[] = entries
+    .filter(
+      (entry) =>
+        entry.date <= date &&
+        entry.date >
+          new Date(new Date().setDate(new Date().getDate() - numberOfDays))
+    )
+    .map((entry) => entry.weight);
+  return (
+    entriesInLastXDays.reduce((acc, val) => acc + val, 0) /
+    entriesInLastXDays.length
+  );
+};
