7ffe001345f64573218fdae034c77c7bb8e0702e
Dimitri CORREIA <125220634+dimitri-correia@users.noreply.github.com>
2023-02-23 22:53:34 +0100

    creating styles for the page

--- styles/CommonStyles.tsx
+++ styles/CommonStyles.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black",
+    backgroundColor: "white",
     paddingHorizontal: 10,
     paddingTop: 20,
   },
