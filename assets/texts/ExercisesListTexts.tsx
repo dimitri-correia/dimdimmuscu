817ed3f2872e10ee2ddd1048c815aba608db3135
Dimitri CORREIA <125220634+dimitri-correia@users.noreply.github.com>
2023-02-25 23:25:30 +0100

    useful text

---
+++ assets/texts/ExercisesListTexts.tsx
@@ -0,0 +1,6 @@
+export const addExercise = "Add Exercise";
+export const exerciseName = "Exercise Name";
+export const primaryMuscleGroup = "Primary Muscle Group";
+export const secondaryMuscleGroup = "Secondary Muscle Group";
+
+export const addMuscleGroup = "Add Muscle Group";
