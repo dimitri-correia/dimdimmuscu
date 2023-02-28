91bdbc770ce1d58048565b6f3188e12fbade4200
Dimitri CORREIA <125220634+dimitri-correia@users.noreply.github.com>
2023-02-28 20:01:12 +0100

    creating the style file

---
+++ styles/PersonalRecordStyles.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  exTable: {
+    paddingTop: 20,
+  },
+  tableName: {
+    fontWeight: "bold",
+  },
+  tableLines: {
+    flexDirection: "row",
+    paddingTop: 5,
+  },
+  tableItem: {
+    flex: 1,
+  },
+});
