21ebc31c4e69f3a6a6cd12404d59d3fac6ef1cdf
Dimitri CORREIA <125220634+dimitri-correia@users.noreply.github.com>
2023-03-04 20:24:48 +0100

    using a new navigator

--- screens/CalculatorsScreen.tsx
+++ screens/CalculatorsScreen.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 import { View } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+const Tab = createMaterialTopTabNavigator();
 
 export const CalculatorsScreen: React.FC = () => {
+  return (
+    <Tab.Navigator initialRouteName={"CalculatorsScreen"}>
+      <Tab.Screen name="1RMCalculator" component={OneRMCalculator} />
+      <Tab.Screen name="WarmUpMaxAttempt" component={WarmUpMaxAttempt} />
+    </Tab.Navigator>
+  );
+};
+
+const OneRMCalculator: React.FC = () => {
+  return <View />;
+};
+const WarmUpMaxAttempt: React.FC = () => {
   return <View />;
 };
