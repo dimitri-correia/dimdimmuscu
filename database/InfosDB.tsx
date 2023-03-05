cd5143bf33f126787cdc79a11ec304cfab6b01f9
Dimitri CORREIA <125220634+dimitri-correia@users.noreply.github.com>
2023-03-05 22:50:02 +0100

    prepopulate the db

--- database/InfosDB.tsx
+++ database/InfosDB.tsx
@@ -10,17 +10,28 @@ export const createInfosTable = () => {
   CommonDB.transaction((tx) => {
     tx.executeSql(
       `CREATE TABLE IF NOT EXISTS ${infoEntries} (
-      ${colId} INTEGER PRIMARY KEY AUTOINCREMENT,
-      ${colFieldName} STRING NOT NULL UNIQUE,
-      ${colFieldValue} STRING NOT NULL);`
+        ${colId} INTEGER PRIMARY KEY AUTOINCREMENT,
+        ${colFieldName} TEXT NOT NULL UNIQUE,
+        ${colFieldValue} TEXT NOT NULL);
+      INSERT INTO ${infoEntries} (${colId}, ${colFieldName}, ${colFieldValue}) 
+      SELECT 1, 'name', 'dim' 
+      WHERE NOT EXISTS (SELECT 1 FROM ${infoEntries} WHERE ${colId} = 1);
+      INSERT INTO ${infoEntries} (${colId}, ${colFieldName}, ${colFieldValue}) 
+      SELECT 2, 'birthdate', '05-05-1999' 
+      WHERE NOT EXISTS (SELECT 1 FROM ${infoEntries} WHERE ${colId} = 2);
+      INSERT INTO ${infoEntries} (${colId}, ${colFieldName}, ${colFieldValue}) 
+      SELECT 3, 'height', '180' 
+      WHERE NOT EXISTS (SELECT 1 FROM ${infoEntries} WHERE ${colId} = 3);`
     );
   });
 };
 
 export const getInfoEntries = (): Promise<Map<number, InfoEntry>> => {
+  console.log("h");
   return new Promise<Map<number, InfoEntry>>((resolve, _) => {
     CommonDB.transaction((tx) => {
       tx.executeSql(`SELECT * FROM ${infoEntries};`, [], (_, result) => {
+        console.log("r", result);
         const infoEntries: Map<number, InfoEntry> = new Map();
         for (let i = 0; i < result.rows.length; i++) {
           const row = result.rows.item(i);
