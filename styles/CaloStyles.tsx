1c71aad366f161e8d2316eb46f2b7c2af099443c
Dimitri CORREIA <125220634+dimitri-correia@users.noreply.github.com>
2023-03-16 19:10:04 +0100

    calo styles

---
+++ styles/CaloStyles.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    backgroundColor: "#DDD",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  row: {
+    flexDirection: "row",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  cell: {
+    flex: 1,
+    textAlign: "center",
+  },
+});
