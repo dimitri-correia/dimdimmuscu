eb86b0b0986aafb047791e9faa76e74dbd086ed1
Dimitri CORREIA <125220634+dimitri-correia@users.noreply.github.com>
2023-03-18 21:48:05 +0100

    views creation

---
+++ screens/CardioTrackerScreen.tsx
@@ -0,0 +1,166 @@
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Modal,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import {
+  addWeight,
+  averageXdays,
+  deleteEntry,
+  editEntry,
+  refreshWeightEntries,
+  WeightEntry,
+} from "../logic/WeightTrackerLogic";
+import CommonStyles, { editWeightStyles } from "../styles/CommonStyles";
+import { addWeightStyles, pageStyles } from "../styles/WeightTrackerStyles";
+import * as TextWT from "../assets/texts/WeightTracker";
+import * as TextCommon from "../assets/texts/Common";
+
+export const CardioTrackerScreen: React.FC = () => {
+  const [weightEntries, setWeightEntries] = useState<WeightEntry[]>([]);
+
+  const [newWeight, setNewWeight] = useState<string>("");
+
+  const [editId, setEditId] = useState<number>(-1); // if -1 modal invisible
+
+  useEffect(() => {
+    refreshWeightEntries(setWeightEntries);
+  }, [editId]);
+
+  return (
+    <View style={CommonStyles.container}>
+      <ModalEditWeight
+        entry={weightEntries.find((we) => we.id == editId)}
+        setNewWeight={setNewWeight}
+        handleEditWeight={() => editEntry(newWeight, editId, setEditId)}
+        handleDeleteWeight={() => deleteEntry(editId, setEditId)}
+      />
+      <View style={addWeightStyles.addWeightContainer}>
+        <Text style={addWeightStyles.addWeightLabel}>{TextWT.enterWeight}</Text>
+        <TextInput
+          style={addWeightStyles.addWeightInput}
+          keyboardType="numeric"
+          onChangeText={(text) => setNewWeight(text)}
+        />
+        <Button
+          title={TextWT.add}
+          onPress={() => {
+            addWeight(weightEntries, newWeight);
+            refreshWeightEntries(setWeightEntries);
+          }}
+        />
+      </View>
+      <Row
+        date={TextWT.date}
+        todayWeight={TextWT.weightInKg}
+        average7days={TextWT.average7}
+        average31days={TextWT.average31}
+      />
+      {weightEntries.map((entry) => (
+        <TouchableOpacity
+          key={entry.date.toISOString()}
+          onPress={() => {
+            setEditId(entry.id);
+          }}
+        >
+          <Row
+            date={entry.date.toDateString()}
+            todayWeight={entry.weight.toFixed(1)}
+            average7days={averageXdays(weightEntries, entry.date, 7).toFixed(1)}
+            average31days={averageXdays(weightEntries, entry.date, 31).toFixed(
+              1
+            )}
+          />
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
+
+interface RowItem {
+  date: string;
+  todayWeight: string;
+  average7days: string;
+  average31days: string;
+}
+
+const Row = ({ date, todayWeight, average7days, average31days }: RowItem) => {
+  return (
+    <View style={pageStyles.row}>
+      <Text style={pageStyles.column}>{date}</Text>
+      <Text style={pageStyles.column}>{todayWeight}</Text>
+      <Text style={pageStyles.column}>{average7days}</Text>
+      <Text style={pageStyles.column}>{average31days}</Text>
+    </View>
+  );
+};
+
+interface ModalEditProps {
+  entry: WeightEntry | undefined;
+  setNewWeight: (value: ((prevState: string) => string) | string) => void;
+  handleEditWeight: () => void;
+  handleDeleteWeight: () => void;
+}
+
+const ModalEditWeight = ({
+  entry,
+  setNewWeight,
+  handleEditWeight,
+  handleDeleteWeight,
+}: ModalEditProps) => {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={entry != undefined}
+    >
+      {entry != undefined && (
+        <View style={editWeightStyles.modal}>
+          <View style={editWeightStyles.background}>
+            <View style={editWeightStyles.line}>
+              <Text style={editWeightStyles.title}>
+                {TextWT.editWeightEntry}
+              </Text>
+            </View>
+            <View style={editWeightStyles.line}>
+              <Text style={editWeightStyles.label}>
+                {`${entry.date.toDateString()}`}
+              </Text>
+              <Text style={editWeightStyles.label}>{`${entry.weight}kg`}</Text>
+            </View>
+            <View style={editWeightStyles.line}>
+              <Text style={editWeightStyles.label}>{TextWT.enterWeight}</Text>
+              <TextInput
+                style={editWeightStyles.input}
+                keyboardType="numeric"
+                onChangeText={(text) => setNewWeight(text)}
+              />
+            </View>
+            <View style={editWeightStyles.line}>
+              <TouchableOpacity
+                style={[editWeightStyles.button, editWeightStyles.buttonOK]}
+                onPress={handleEditWeight}
+              >
+                <Text style={editWeightStyles.buttonText}>
+                  {TextCommon.save}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[editWeightStyles.button, editWeightStyles.buttonKO]}
+                onPress={handleDeleteWeight}
+              >
+                <Text style={editWeightStyles.buttonText}>
+                  {TextCommon.toDelete}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      )}
+    </Modal>
+  );
+};
