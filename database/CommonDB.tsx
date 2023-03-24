e61a86e249873301015a42467bb5533fd15fac18
Dimitri CORREIA <125220634+dimitri-correia@users.noreply.github.com>
2023-03-24 12:47:15 +0100

    remove cycle in import

--- database/CommonDB.tsx
+++ database/CommonDB.tsx
@@ -1,20 +1,3 @@
 import { openDatabase } from "expo-sqlite";
-import { createWeightTable } from "./WeightTrackerDB";
-import { createExercisesTableS } from "./ExercisesListDB";
-import { createPRTable } from "./PersonalRecordDB";
-import { createInfosTable } from "./InfosDB";
-import { createImageTable } from "./PictureEvolutionDB";
-import { createCardioTable } from "./CardioDB";
-import { createCaloriesTable } from "./CaloriesDB";
 
 export default openDatabase("dimdimmuscu.db");
-
-export function createAllTables() {
-  createWeightTable();
-  createExercisesTableS();
-  createPRTable();
-  createInfosTable();
-  createImageTable();
-  createCardioTable();
-  createCaloriesTable();
-}
