094d27ad0bdcac7f05eebd59898a02458a2360de
Dimitri CORREIA <125220634+dimitri-correia@users.noreply.github.com>
2023-03-24 14:35:47 +0100

    create a chronometer component

--- screens/SessionTrackerScreen.tsx
+++ screens/SessionTrackerScreen.tsx
@@ -1,13 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { Button, Text, TextInput, View } from "react-native";
-import {
-  addCardio,
-  CardioEntry,
-  refreshCardioEntries,
-} from "../logic/CardioLogic";
+import React, { useEffect, useRef, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { CardioEntry, refreshCardioEntries } from "../logic/CardioLogic";
 import CommonStyles from "../styles/CommonStyles";
-import { addWeightStyles, pageStyles } from "../styles/WeightTrackerStyles";
+import { pageStyles } from "../styles/WeightTrackerStyles";
 import * as TextWT from "../assets/texts/WeightTracker";
+import { Audio } from "expo-av";
 
 interface Set {
   setNumber: number;
@@ -29,30 +26,7 @@ export const SessionTrackerScreen: React.FC = () => {
 
   return (
     <View style={CommonStyles.container}>
-      <View style={addWeightStyles.addWeightContainer}>
-        <Text style={addWeightStyles.addWeightLabel}>{TextWT.enterWeight}</Text>
-        <TextInput
-          style={addWeightStyles.addWeightInput}
-          onChangeText={(text) => setNewName(text)}
-        />
-        <TextInput
-          style={addWeightStyles.addWeightInput}
-          keyboardType="numeric"
-          onChangeText={(text) => setNewTime(text)}
-        />
-        <TextInput
-          style={addWeightStyles.addWeightInput}
-          keyboardType="numeric"
-          onChangeText={(text) => setNewCalo(text)}
-        />
-        <Button
-          title={TextWT.add}
-          onPress={() => {
-            addCardio(newName, newTime, newCalo);
-            refreshCardioEntries(setCardioEntries);
-          }}
-        />
-      </View>
+      <Chronometer />
       <Row
         date={TextWT.date}
         name={TextWT.weightInKg}
@@ -85,6 +59,67 @@ const Row = ({ date, name, time, calo }: RowItem) => {
       <Text style={pageStyles.column}>{name}</Text>
       <Text style={pageStyles.column}>{time}</Text>
       <Text style={pageStyles.column}>{calo}</Text>
+    </View>
+  );
+};
+
+const Chronometer = () => {
+  const [time, setTime] = useState<number>(0);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+  };
+
+  const playSound = (): void => {
+    const soundObject = new Audio.Sound();
+    try {
+      soundObject
+        .loadAsync(require("../assets/Songs/30sNotif.mp3"))
+        .then(() => {
+          soundObject.playAsync().catch(() => {
+            console.log("Error playing 30s notif");
+          });
+        });
+    } catch (error) {
+      console.warn("Error playing 30s notif", error);
+    }
+  };
+
+  const startTimer = () => {
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
+      setTime((prevTime) => {
+        if ((prevTime + 1) % 30 === 0) {
+          playSound();
+        }
+        return prevTime + 1;
+      });
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    setIsRunning(false);
+    clearInterval(intervalRef.current as NodeJS.Timeout);
+  };
+
+  const resetTimer = () => {
+    stopTimer();
+    setTime(0);
+  };
+
+  return (
+    <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+      <Text>{formatTime(time)}</Text>
+      <TouchableOpacity onPress={isRunning ? stopTimer : startTimer}>
+        <Text>{isRunning ? "Stop" : "Start"}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={resetTimer}>
+        <Text>Reset</Text>
+      </TouchableOpacity>
     </View>
   );
 };
