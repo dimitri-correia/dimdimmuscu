b56161803dcf08a8ea022574bbd971c0b4ed1156
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-17 23:49:17 +0100

    init db

---
+++ back/dimdimmuscuback/src/db/methods/init_db.rs
@@ -0,0 +1,13 @@
+use shuttle_runtime::CustomError;
+use sqlx::PgPool;
+
+use crate::db::structs::db_infos::DbInfos;
+
+pub async fn init_db(pool: PgPool) -> DbInfos {
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .map_err(CustomError::new)?;
+
+    DbInfos { pool }
+}
