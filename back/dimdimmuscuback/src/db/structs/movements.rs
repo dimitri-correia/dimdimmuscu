35302ef94f6c9abe9d44a4487e7af21d0c2e77f1
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-17 23:40:05 +0100

    table movements

---
+++ back/dimdimmuscuback/src/db/structs/movements.rs
@@ -0,0 +1,50 @@
+use sqlx::{Error, PgPool};
+
+#[derive(Debug, sqlx::FromRow)]
+pub struct Movement {
+    id: i32,
+    name: String,
+    first_muscle_id: i32,
+    second_muscle_id: i32,
+    third_muscle_id: i32,
+}
+
+impl Movement {
+    async fn create_new(
+        pool: &PgPool,
+        name: &str,
+        first_muscle_id: i32,
+        second_muscle_id: i32,
+        third_muscle_id: i32,
+    ) -> Result<Self, Error> {
+        let row = sqlx::query_as::<_, Self>(
+            "INSERT INTO movements (name, first_muscle, second_muscle, third_muscle) VALUES ($1, $2, $3, $4)")
+            .bind(name).bind(first_muscle_id).bind(second_muscle_id).bind(third_muscle_id)
+            .fetch_one(pool)
+            .await?;
+        Ok(row)
+    }
+
+    async fn find_all(pool: &PgPool) -> Result<Vec<Self>, Error> {
+        let row = sqlx::query_as::<_, Self>("SELECT * FROM movements")
+            .fetch_all(pool)
+            .await?;
+        Ok(row)
+    }
+
+    async fn find_by_name(pool: &PgPool, name: &str) -> Result<Self, Error> {
+        let row = sqlx::query_as::<_, Self>("SELECT * FROM movements WHERE name = $1")
+            .bind(name)
+            .fetch_one(pool)
+            .await?;
+        Ok(row)
+    }
+
+    async fn find_by_id(pool: &PgPool, id: i32) -> Result<Self, Error> {
+        let row = sqlx::query_as::<_, Self>("SELECT * FROM movements WHERE id = $1")
+            .bind(id)
+            .fetch_one(pool)
+            .await?;
+        Ok(row)
+    }
+}
