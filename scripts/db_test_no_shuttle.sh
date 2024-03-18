a6f9f075cffc10c925d26052bf625791af018cc0
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-18 23:24:40 +0100

    db creation script

---
+++ scripts/db_test_no_shuttle.sh
@@ -0,0 +1,8 @@
+# will be launched automatically by tests
+docker kill postgres_test_without_shuttle
+docker rm postgres_test_without_shuttle
+docker run \
+  -e POSTGRES_USER=dim  -e POSTGRES_PASSWORD=dim -e POSTGRES_DB=db_for_tests_without_shuttle \
+  -p 3269:5432 -d \
+  --name "postgres_test_without_shuttle" \
+  postgres
