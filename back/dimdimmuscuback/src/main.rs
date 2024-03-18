e7a31a4a524dead80f77f8529fff8bbb4cb7f58a
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-18 01:36:30 +0100

    tracing and cookies

--- back/dimdimmuscuback/src/main.rs
+++ back/dimdimmuscuback/src/main.rs
@@ -1,18 +1,28 @@
 use axum::Router;
+use shuttle_runtime::__internals::tracing_subscriber;
 use sqlx::PgPool;
+use tower_cookies::CookieManagerLayer;
+use tracing::info;
 
 use crate::db::methods::init_db::init_db;
 use crate::routers::fallback::fallback;
 
+mod cookies;
 mod db;
+mod errors;
 mod logic;
 mod routers;
 
 #[shuttle_runtime::main]
 async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
+    tracing_subscriber::fmt().init();
+
     let router = Router::new()
         .fallback(fallback)
+        .layer(CookieManagerLayer::new())
         .with_state(init_db(pool).await);
+
+    info!("started");
 
     Ok(router.into())
 }
