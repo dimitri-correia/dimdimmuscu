81ffcf6180097347e14adfa0a3f289a1203f0c1f
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-18 01:29:44 +0100

    lib envs

---
+++ back/libs/lib-envs/src/lib.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::str::FromStr;
+
+use lib_b64::b64u_decode;
+
+pub fn get_env(name: &'static str) -> Result<String, EnvError> {
+    env::var(name).map_err(|_| EnvError::MissingEnv(name))
+}
+
+pub fn get_env_parse<T: FromStr>(name: &'static str) -> Result<T, EnvError> {
+    let val = get_env(name)?;
+    val.parse::<T>().map_err(|_| EnvError::WrongFormat(name))
+}
+
+pub fn get_env_b64u_as_u8s(name: &'static str) -> Result<Vec<u8>, EnvError> {
+    b64u_decode(&get_env(name)?).map_err(|_| EnvError::WrongFormat(name))
+}
+
+#[derive(Debug)]
+pub enum EnvError {
+    MissingEnv(&'static str),
+    WrongFormat(&'static str),
+}
