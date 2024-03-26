69186c28eab50ff74c807752a40bc9e11e6eb284
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-26 22:47:13 +0100

    init db without script

--- back/dimdimmuscuback/src/db/methods/init_db.rs
+++ back/dimdimmuscuback/src/db/methods/init_db.rs
@@ -1,56 +1,80 @@
-use std::process::Command;
-
-use shuttle_runtime::CustomError;
 use sqlx::postgres::PgPool;
-use tokio::sync::OnceCell;
-
-pub async fn init_db_shuttle(pool: PgPool) -> DbInfos {
-    sqlx::migrate!()
-        .run(&pool)
-        .await
-        .map_err(CustomError::new)
-        .expect("cannot init database");
-
-    DbInfos { pool }
-}
 
 #[derive(Clone)]
 pub struct DbInfos {
     pub pool: PgPool,
 }
 
-pub async fn init_test() -> DbInfos {
-    static INIT: OnceCell<DbInfos> = OnceCell::const_new();
-
-    INIT.get_or_init(|| async { prepare_db().await })
+pub async fn init_db(pool: &PgPool) {
+    sqlx::migrate!()
+        .run(pool)
         .await
-        .clone()
+        .expect("Cannot init database");
 }
 
-async fn prepare_db() -> DbInfos {
-    launch_docker_db();
-    let db = setup_db().await;
-    populate_db(&db).await;
+#[cfg(test)]
+pub mod base_tests_db {
+    use std::thread::sleep;
+    use std::time;
+    use std::time::Duration;
 
-    db
-}
+    use sqlx::{Error, PgPool};
+    use testcontainers::clients::Cli;
+    use testcontainers_modules::postgres::Postgres;
+    use tokio::sync::OnceCell;
 
-fn launch_docker_db() {
-    let e = Command::new("sh")
-        .arg("../../scripts/db_test_no_shuttle.sh")
-        .output()
-        .expect("failed to execute process");
-    if e.stdout.is_empty() || !e.stderr.is_empty() {
-        panic!("Failed to create db");
+    use crate::db::methods::init_db::{init_db, DbInfos};
+
+    pub async fn init_test() -> DbInfos {
+        static INIT: OnceCell<DbInfos> = OnceCell::const_new();
+
+        INIT.get_or_init(|| async { prepare_db().await })
+            .await
+            .clone()
+    }
+
+    pub async fn check_db_health(pool: &PgPool) -> Result<(), Error> {
+        sqlx::query("SELECT 1").execute(pool).await.map(|_| ())
+    }
+
+    async fn prepare_db() -> DbInfos {
+        let db = setup_db().await;
+        dbg!("a");
+        sleep(time::Duration::from_secs_f32(3f32));
+        dbg!("b");
+        for _ in 0..5 {
+            if let Ok(_) = check_db_health(&db.pool).await {
+                break;
+            }
+            tokio::time::sleep(Duration::from_secs(5)).await;
+        }
+
+        init_db(&db.pool).await;
+
+        // Check the database health
+        check_db_health(&db.pool)
+            .await
+            .expect("Database health check failed");
+
+        db
+    }
+
+    async fn setup_db() -> DbInfos {
+        let docker = Cli::default();
+        let node = docker.run(Postgres::default());
+
+        // prepare connection string
+        let connection_string = &format!(
+            "postgres://postgres:postgres@127.0.0.1:{}/postgres",
+            node.get_host_port_ipv4(5432)
+        );
+
+        let pool: PgPool = PgPool::connect(connection_string).await.unwrap();
+
+        DbInfos { pool }
+    }
+
+    async fn populate_db(pool: &PgPool) {
+        dbg!("c");
     }
 }
-
-async fn setup_db() -> DbInfos {
-    let pool = PgPool::connect("postgresql://dim:dim@localhost:3269/db_for_tests_without_shuttle")
-        .await
-        .expect("Failed to connect to the database for testing");
-
-    DbInfos { pool }
-}
-
-async fn populate_db(db: &DbInfos) {}
--- back/dimdimmuscuback/src/main.rs
+++ back/dimdimmuscuback/src/main.rs
@@ -1,10 +1,7 @@
-use axum::{middleware, Router};
-use shuttle_runtime::__internals::tracing_subscriber;
+use axum::Router;
 use sqlx::PgPool;
-use tower_cookies::CookieManagerLayer;
-use tracing::info;
 
-use crate::db::methods::init_db::init_db_shuttle;
+use crate::db::methods::init_db::{init_db, DbInfos};
 use crate::routers::fallback::fallback;
 use crate::routers::login::login_routes;
 
@@ -17,21 +14,19 @@ mod routers;
 
 #[shuttle_runtime::main]
 async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
-    tracing_subscriber::fmt().init();
+    init_db(&pool).await;
 
-    let db = init_db_shuttle(pool).await;
+    let db = DbInfos { pool };
 
     let router = Router::new()
         .nest("/connect", login_routes(db.clone()))
-        .nest("/api", routes_connected())
-        .fallback(fallback)
-        .layer(CookieManagerLayer::new());
-
-    info!("started");
+        // .nest("/api", routes_connected())
+        .fallback(fallback);
+    //.layer(CookieManagerLayer::new());
 
     Ok(router.into())
 }
 
-fn routes_connected() -> Router {
-    todo!()
-}
+// fn routes_connected() -> Router {
+//     todo!()
+// }
