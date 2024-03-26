c969801d02333fbc1c472f4467186f07703d04c0
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-26 22:46:19 +0100

    tmp remove

--- back/dimdimmuscuback/src/cookies/mod.rs
+++ back/dimdimmuscuback/src/cookies/mod.rs
@@ -1,25 +1,20 @@
-use uuid::Uuid;
-
-use lib_auth::token::{error::AuthError, generate_web_token};
-use tower_cookies::{Cookie, Cookies};
-
 pub const AUTH_TOKEN: &str = "auth-token";
 
-pub fn set_token_cookie(cookies: &Cookies, user: &str, salt: Uuid) -> Result<(), AuthError> {
-    let token = generate_web_token(user, salt)?;
-
-    let mut cookie = Cookie::new(AUTH_TOKEN, token.to_string());
-    cookie.set_http_only(true);
-    cookie.set_path("/");
-
-    cookies.add(cookie);
-
-    Ok(())
-}
-
-pub fn remove_token_cookie(cookies: &Cookies) {
-    let mut cookie = Cookie::from(AUTH_TOKEN);
-    cookie.set_path("/");
-
-    cookies.remove(cookie);
-}
+// pub fn set_token_cookie(cookies: &Cookies, user: &str, salt: Uuid) -> Result<(), AuthError> {
+//     let token = generate_web_token(user, salt)?;
+//
+//     let mut cookie = Cookie::new(AUTH_TOKEN, token.to_string());
+//     cookie.set_http_only(true);
+//     cookie.set_path("/");
+//
+//     cookies.add(cookie);
+//
+//     Ok(())
+// }
+//
+// pub fn remove_token_cookie(cookies: &Cookies) {
+//     let mut cookie = Cookie::from(AUTH_TOKEN);
+//     cookie.set_path("/");
+//
+//     cookies.remove(cookie);
+// }
--- back/dimdimmuscuback/src/mw/mw_auth.rs
+++ back/dimdimmuscuback/src/mw/mw_auth.rs
@@ -1,61 +1,47 @@
-use axum::body::Body;
-use axum::extract::State;
-use axum::http::Request;
-use axum::middleware::Next;
-use axum::response::Response;
-use tower_cookies::{Cookie, Cookies};
-use tracing::debug;
-
-use lib_auth::token::{validate_web_token, Token};
-
-use crate::cookies::{set_token_cookie, AUTH_TOKEN};
-use crate::db::methods::init_db::DbInfos;
-use crate::db::structs::users::UserForLogin;
-
-pub async fn mw_ctx_resolver(
-    State(db): State<DbInfos>,
-    cookies: Cookies,
-    mut req: Request<Body>,
-    next: Next,
-) -> Response {
-    debug!("{:<12} - mw_ctx_resolve", "MIDDLEWARE");
-
-    let ctx_ext_result = ctx_resolve(db, &cookies).await;
-
-    if ctx_ext_result.is_err() && !matches!(ctx_ext_result, Err(CtxExtError::TokenNotInCookie)) {
-        cookies.remove(Cookie::from(AUTH_TOKEN))
-    }
-
-    req.extensions_mut().insert(ctx_ext_result);
-
-    next.run(req).await
-}
-
-async fn ctx_resolve(db_infos: DbInfos, cookies: &Cookies) -> Result {
-    // -- Get Token String
-    let token = cookies
-        .get(AUTH_TOKEN)
-        .map(|c| c.value().to_string())
-        .ok_or(CtxExtError::TokenNotInCookie)?;
-
-    // -- Parse Token
-    let token: Token = token.parse().map_err(|_| CtxExtError::TokenWrongFormat)?;
-
-    // -- Get UserForAuth
-    let user: UserForLogin = UserForLogin::find_by_name(&db_infos, &token.ident)
-        .await
-        .map_err(|ex| CtxExtError::ModelAccessError(ex.to_string()))?
-        .ok_or(CtxExtError::UserNotFound)?;
-
-    // -- Validate Token
-    validate_web_token(&token, user.token_salt).map_err(|_| CtxExtError::FailValidate)?;
-
-    // -- Update Token
-    set_token_cookie(cookies, &user.username, user.token_salt)
-        .map_err(|_| CtxExtError::CannotSetTokenCookie)?;
-
-    // -- Create CtxExtResult
-    Ctx::new(user.id)
-        .map(CtxW)
-        .map_err(|ex| CtxExtError::CtxCreateFail(ex.to_string()))
-}
+// pub async fn mw_ctx_resolver(
+//     State(db): State<DbInfos>,
+//     cookies: Cookies,
+//     mut req: Request<Body>,
+//     next: Next,
+// ) -> Response {
+//     debug!("{:<12} - mw_ctx_resolve", "MIDDLEWARE");
+//
+//     let ctx_ext_result = ctx_resolve(db, &cookies).await;
+//
+//     if ctx_ext_result.is_err() && !matches!(ctx_ext_result, Err(CtxExtError::TokenNotInCookie)) {
+//         cookies.remove(Cookie::from(AUTH_TOKEN))
+//     }
+//
+//     req.extensions_mut().insert(ctx_ext_result);
+//
+//     next.run(req).await
+// }
+//
+// async fn ctx_resolve(db_infos: DbInfos, cookies: &Cookies) -> Result {
+//     // -- Get Token String
+//     let token = cookies
+//         .get(AUTH_TOKEN)
+//         .map(|c| c.value().to_string())
+//         .ok_or(CtxExtError::TokenNotInCookie)?;
+//
+//     // -- Parse Token
+//     let token: Token = token.parse().map_err(|_| CtxExtError::TokenWrongFormat)?;
+//
+//     // -- Get UserForAuth
+//     let user: UserForLogin = UserForLogin::find_by_name(&db_infos, &token.ident)
+//         .await
+//         .map_err(|ex| CtxExtError::ModelAccessError(ex.to_string()))?
+//         .ok_or(CtxExtError::UserNotFound)?;
+//
+//     // -- Validate Token
+//     validate_web_token(&token, user.token_salt).map_err(|_| CtxExtError::FailValidate)?;
+//
+//     // -- Update Token
+//     set_token_cookie(cookies, &user.username, user.token_salt)
+//         .map_err(|_| CtxExtError::CannotSetTokenCookie)?;
+//
+//     // -- Create CtxExtResult
+//     Ctx::new(user.id)
+//         .map(CtxW)
+//         .map_err(|ex| CtxExtError::CtxCreateFail(ex.to_string()))
+// }
