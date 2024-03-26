7a485bb1deaec08a8a999ad89208d0674f05125d
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-26 22:45:41 +0100

    clean fallback

--- back/dimdimmuscuback/src/routers/fallback.rs
+++ back/dimdimmuscuback/src/routers/fallback.rs
@@ -1,5 +1,8 @@
 use axum::http::StatusCode;
 
 pub async fn fallback() -> (StatusCode, String) {
-    (StatusCode::NOT_FOUND, "Error".to_string())
+    (
+        StatusCode::NOT_FOUND,
+        "Error, nothing to return here.".to_string(),
+    )
 }
