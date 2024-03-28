9abda3745c21e81c840e9f24778cbdad70096d0c
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-28 23:46:41 +0100

    init server clean

--- back/dimdimmuscuback/src/main.rs
+++ back/dimdimmuscuback/src/main.rs
@@ -1,6 +1,5 @@
 use axum::Router;
 
-use crate::db::methods::init_db::init_db;
 use crate::env::init_env;
 use crate::routers::auth::auth_routes;
 use crate::routers::fallback::fallback;
@@ -14,11 +13,10 @@ mod routers;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let (env_variables, env_variables_db) = init_env();
-    let db = init_db(env_variables_db).await;
+    let env_variables = init_env().await;
 
     let router = Router::new()
-        .nest("/connect", auth_routes(db.clone()))
+        .nest("/connect", auth_routes(&env_variables))
         // .nest("/api", routes_connected())
         .fallback(fallback);
     //.layer(CookieManagerLayer::new());
