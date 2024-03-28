30694365d697fcda0c0ce91d1d597fd95fdd0a73
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-29 00:16:39 +0100

    session wip

--- back/dimdimmuscuback/src/db/structs/session.rs
+++ back/dimdimmuscuback/src/db/structs/session.rs
@@ -1,27 +1,46 @@
-use chrono::{DateTime, Local, Utc};
-use sqlx::{FromRow, PgPool};
+use std::i32;
 
-use crate::errors::cookies::CookieError;
+use chrono::{DateTime, Utc};
+use jsonwebtoken::{encode, EncodingKey, Header};
+use libsql::Connection;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, FromRow)]
+use crate::errors::auth::session::SessionError;
+
+#[derive(Debug)]
 pub struct Session {
     token: String,
-    id: i64,
+    profile_id: i32,
+    until: DateTime<Utc>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct SessionToken {
+    profile_id: i32,
     until: DateTime<Utc>,
 }
 
 impl Session {
-    pub async fn new(profile_id: i64, pool: &PgPool) -> Result<String, CookieError> {
-        let token_for_auth = String::new();
+    pub async fn create(profile_id: i32, conn: &Connection) -> Result<String, SessionError> {
+        let until = Utc::now(); //+ Duration::hours(session_duration_hours);
 
-        sqlx::query("INSERT INTO session (token, profile_id, until) VALUES ($1, $2, $3)")
-            .bind(&token_for_auth)
-            .bind(profile_id)
-            .bind(Local::now())
-            .execute(pool)
-            .await
-            .map_err(CookieError::ErrorWithDb)?; // need more precision;
+        let session_token = SessionToken { profile_id, until };
 
-        Ok(token_for_auth)
+        let token = encode(
+            &Header::default(),
+            &session_token,
+            &EncodingKey::from_secret(b"your_secret_key"),
+        )
+        .map_err(|_| SessionError::TokenCreation)?;
+
+        // sqlx::query("INSERT INTO session (token, profile_id, until) VALUES ($1, $2, $3)")
+        //     .bind(&token)
+        //     .bind(profile_id)
+        //     .bind(until)
+        //     .execute(pool)
+        //     .await
+        //     .map_err(CookieError::ErrorWithDb)?; // need more precision;
+
+        Ok(token)
     }
 }
