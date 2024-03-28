1b8f9792866c484d2ddb31c74cf03e83164caa1b
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-03-28 23:47:15 +0100

    rnv clean

--- back/dimdimmuscuback/src/env/mod.rs
+++ back/dimdimmuscuback/src/env/mod.rs
@@ -1,19 +1,36 @@
 use std::env;
 
+use libsql::Connection;
 use rand::{rngs::OsRng, RngCore};
 
-pub struct EnvVariables {
-    session_duration_hours: i64,
-    secret_key_session: [u8; 32],
-}
+use crate::db::methods::init_db::init_db;
 
-pub struct EnvVariablesDb {
-    pub db_url: String,
-    pub db_auth_token: String,
+#[derive(Clone)]
+pub struct EnvVariables {
+    pub session_duration_hours: i64,
+    pub secret_key_session: [u8; 32],
+    pub db_connection: Connection,
 }
 
 // Should fail directly in case of issue
-pub fn init_env() -> (EnvVariables, EnvVariablesDb) {
+pub async fn init_env() -> EnvVariables {
+    // from .env
+    let (session_duration_hours, db_url, db_auth_token) = get_variables_from_env();
+
+    // need to change at each restart
+    let secret_key_session = generate_secret_key();
+
+    // turso connection
+    let db_connection = init_db(db_url, db_auth_token).await;
+
+    EnvVariables {
+        session_duration_hours,
+        secret_key_session,
+        db_connection,
+    }
+}
+
+fn get_variables_from_env() -> (i64, String, String) {
     let session_duration_hours: i64 = env::var("session_duration_hours")
         .expect("")
         .parse()
@@ -21,19 +38,7 @@ pub fn init_env() -> (EnvVariables, EnvVariablesDb) {
 
     let db_url = env::var("LIBSQL_URL").expect("LIBSQL_URL must be set");
     let db_auth_token = env::var("LIBSQL_AUTH_TOKEN").unwrap_or_default();
-
-    let secret_key_session = generate_secret_key(); // need to change at each restart
-
-    (
-        EnvVariables {
-            session_duration_hours,
-            secret_key_session,
-        },
-        EnvVariablesDb {
-            db_url,
-            db_auth_token,
-        },
-    )
+    (session_duration_hours, db_url, db_auth_token)
 }
 
 fn generate_secret_key() -> [u8; 32] {
