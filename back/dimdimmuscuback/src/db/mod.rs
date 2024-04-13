6542e0906145dc8a60af7c2d6df73dadf388962f
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-04-13 21:27:44 +0200

    table info in one place

--- back/dimdimmuscuback/src/db/mod.rs
+++ back/dimdimmuscuback/src/db/mod.rs
@@ -1,2 +1,9 @@
 pub mod methods;
 pub mod structs;
+
+const USERS_TABLE: &str = "users";
+const USERS_TABLE_COL: [&str; 4] = ["id", "name", "birthdate", "account_creation"];
+const USERS_AUTH_TABLE: &str = "users_auth";
+const USERS_AUTH_TABLE_COL: [&str; 2] = ["profile_id", "pwd"];
+const SESSION_TABLE: &str = "session";
+const SESSION_TABLE_COL: [&str; 3] = ["token", "profile_id", "until"];
