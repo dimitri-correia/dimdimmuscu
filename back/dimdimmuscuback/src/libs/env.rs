4f6b5dfd6a806218d3c4431a640d75c9e1524909
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-05-05 21:48:30 +0200

    print to see the issue in ci

--- back/dimdimmuscuback/src/libs/env.rs
+++ back/dimdimmuscuback/src/libs/env.rs
@@ -43,12 +43,8 @@ fn get_variables_from_env() -> (i64, Secret<String>, Secret<String>) {
         Secret::new(env::var("TURSO_AUTH_TOKEN").expect("TURSO_AUTH_TOKEN must be set"));
     let session_duration_hours = env::var("SESSION_DURATION_HOURS")
         .expect("SESSION_DURATION_HOURS must be set")
-        .parse();
-
-    println!("{:?}", session_duration_hours);
-
-    let session_duration_hours =
-        session_duration_hours.expect("SESSION_DURATION_HOURS must be i64");
+        .parse()
+        .expect("SESSION_DURATION_HOURS must be i64");
 
     (session_duration_hours, db_url, db_auth_token)
 }
