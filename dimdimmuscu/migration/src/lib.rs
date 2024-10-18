5791c95b2372af1dc0daff4cb0c00b6b6cf21068
Dimitri CORREIA <dimitri@gymglish.com>
2024-10-19 00:45:32 +0200

    migration only

--- dimdimmuscu/migration/src/lib.rs
+++ dimdimmuscu/migration/src/lib.rs
@@ -5,6 +5,7 @@ pub use sea_orm_migration::prelude::*;
 mod m20220101_000001_users;
 mod m20231103_114510_notes;
 
+mod m20241018_223143_update_users;
 pub struct Migrator;
 
 #[async_trait::async_trait]
@@ -13,6 +14,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20220101_000001_users::Migration),
             Box::new(m20231103_114510_notes::Migration),
+            Box::new(m20241018_223143_update_users::Migration),
         ]
     }
 }
--- dimdimmuscu/tests/models/mod.rs
+++ dimdimmuscu/tests/models/mod.rs
@@ -1,1 +1,3 @@
 mod users;
+
+mod update_users;
