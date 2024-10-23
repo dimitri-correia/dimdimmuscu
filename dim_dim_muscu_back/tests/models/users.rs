19348701cda7e780c10e67eaa656da47114bdeae
Dimitri CORREIA <dimitri@gymglish.com>
2024-10-23 14:41:41 +0200

    clean tests

--- dim_dim_muscu_back/tests/models/users.rs
+++ dim_dim_muscu_back/tests/models/users.rs
@@ -1,10 +1,10 @@
-use insta::assert_debug_snapshot;
-use loco_rs::{model::ModelError, testing};
 use dim_dim_muscu_back::{
     app::App,
     models::users::{self, Model, RegisterParams},
 };
-use sea_orm::{ActiveModelTrait, ActiveValue, IntoActiveModel};
+use insta::assert_debug_snapshot;
+use loco_rs::{model::ModelError, testing};
+use sea_orm::{ActiveModelTrait, ActiveValue};
 use serial_test::serial;
 
 macro_rules! configure_insta {
@@ -106,118 +106,4 @@ async fn can_find_by_pid() {
 
     assert_debug_snapshot!(existing_user);
     assert_debug_snapshot!(non_existing_user_results);
-}
-
-#[tokio::test]
-#[serial]
-async fn can_verification_token() {
-    configure_insta!();
-
-    let boot = testing::boot_test::<App>().await.unwrap();
-    testing::seed::<App>(&boot.app_context.db).await.unwrap();
-
-    let user = Model::find_by_pid(&boot.app_context.db, "11111111-1111-1111-1111-111111111111")
-        .await
-        .unwrap();
-
-    assert!(user.email_verification_sent_at.is_none());
-    assert!(user.email_verification_token.is_none());
-
-    assert!(user
-        .into_active_model()
-        .set_email_verification_sent(&boot.app_context.db)
-        .await
-        .is_ok());
-
-    let user = Model::find_by_pid(&boot.app_context.db, "11111111-1111-1111-1111-111111111111")
-        .await
-        .unwrap();
-
-    assert!(user.email_verification_sent_at.is_some());
-    assert!(user.email_verification_token.is_some());
-}
-
-#[tokio::test]
-#[serial]
-async fn can_set_forgot_password_sent() {
-    configure_insta!();
-
-    let boot = testing::boot_test::<App>().await.unwrap();
-    testing::seed::<App>(&boot.app_context.db).await.unwrap();
-
-    let user = Model::find_by_pid(&boot.app_context.db, "11111111-1111-1111-1111-111111111111")
-        .await
-        .unwrap();
-
-    assert!(user.reset_sent_at.is_none());
-    assert!(user.reset_token.is_none());
-
-    assert!(user
-        .into_active_model()
-        .set_forgot_password_sent(&boot.app_context.db)
-        .await
-        .is_ok());
-
-    let user = Model::find_by_pid(&boot.app_context.db, "11111111-1111-1111-1111-111111111111")
-        .await
-        .unwrap();
-
-    assert!(user.reset_sent_at.is_some());
-    assert!(user.reset_token.is_some());
-}
-
-#[tokio::test]
-#[serial]
-async fn can_verified() {
-    configure_insta!();
-
-    let boot = testing::boot_test::<App>().await.unwrap();
-    testing::seed::<App>(&boot.app_context.db).await.unwrap();
-
-    let user = Model::find_by_pid(&boot.app_context.db, "11111111-1111-1111-1111-111111111111")
-        .await
-        .unwrap();
-
-    assert!(user.email_verified_at.is_none());
-
-    assert!(user
-        .into_active_model()
-        .verified(&boot.app_context.db)
-        .await
-        .is_ok());
-
-    let user = Model::find_by_pid(&boot.app_context.db, "11111111-1111-1111-1111-111111111111")
-        .await
-        .unwrap();
-
-    assert!(user.email_verified_at.is_some());
-}
-
-#[tokio::test]
-#[serial]
-async fn can_reset_password() {
-    configure_insta!();
-
-    let boot = testing::boot_test::<App>().await.unwrap();
-    testing::seed::<App>(&boot.app_context.db).await.unwrap();
-
-    let user = Model::find_by_pid(&boot.app_context.db, "11111111-1111-1111-1111-111111111111")
-        .await
-        .unwrap();
-
-    assert!(user.verify_password("12341234"));
-
-    assert!(user
-        .clone()
-        .into_active_model()
-        .reset_password(&boot.app_context.db, "new-password")
-        .await
-        .is_ok());
-
-    assert!(
-        Model::find_by_pid(&boot.app_context.db, "11111111-1111-1111-1111-111111111111")
-            .await
-            .unwrap()
-            .verify_password("new-password")
-    );
 }
